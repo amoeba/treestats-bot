6dc4df2fe76c5f8f25f58f6712fe2b13bf888c52
Bryce Mecum <petridish@gmail.com>
2025-11-28 21:46:18 -0800

    Fix env var name

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ mod bot;
 async fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
 
-    let token = std::env::var("DISCORD_OAUTH_TOKEN=")
-        .map_err(|e| format!("Failed to get DISCORD_OAUTH_TOKEN=: {}", e))?;
+    let token = std::env::var("DISCORD_OAUTH_TOKEN")
+        .map_err(|e| format!("Failed to get DISCORD_OAUTH_TOKEN: {}", e))?;
     let port = std::env::var("PORT").unwrap_or_else(|_| "3000".to_string());
     let web_url = std::env::var("WEB_URL")
         .unwrap_or_else(|_| format!("http://localhost:{}", port).to_string());
